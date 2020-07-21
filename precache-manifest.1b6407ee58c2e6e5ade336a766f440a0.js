6b19ee57d405c99531385afb383469c15a26ec7c
Simon <mySimonID@gmail.com>
2020-07-21 16:52:24 +0100

    Updates

---
+++ precache-manifest.1b6407ee58c2e6e5ade336a766f440a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87b4832e4dd7038b0798d5317858f506",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "5c174f40f123597dc2a6",
+    "url": "/ReactPortfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e53a4cd99132d0325e19",
+    "url": "/ReactPortfolio/static/css/main.fbf59de6.chunk.css"
+  },
+  {
+    "revision": "5c174f40f123597dc2a6",
+    "url": "/ReactPortfolio/static/js/2.de971869.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactPortfolio/static/js/2.de971869.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e53a4cd99132d0325e19",
+    "url": "/ReactPortfolio/static/js/main.967e3f5c.chunk.js"
+  },
+  {
+    "revision": "0114f5eceb8464d5d956",
+    "url": "/ReactPortfolio/static/js/runtime-main.5b3ce1ae.js"
+  }
+]);
