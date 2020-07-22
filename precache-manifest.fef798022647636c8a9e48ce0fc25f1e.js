1b8603efee8a36089134957e64acf51e5172570d
Simon <mySimonID@gmail.com>
2020-07-22 08:13:50 +0100

    Updates

--- precache-manifest.fef798022647636c8a9e48ce0fc25f1e.js
+++ precache-manifest.fef798022647636c8a9e48ce0fc25f1e.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f54a680d6a7625acc78aa55f392209a1",
+    "revision": "f96b293b219d4654519f2f577099d577",
     "url": "/ReactPortfolio/index.html"
   },
   {
-    "revision": "98d318ea3c6c9699c2d4",
+    "revision": "e2fb2f3d1b80a89a8580",
     "url": "/ReactPortfolio/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "be652f4ad7e55207b1b7",
+    "revision": "566e34d551d8a8cf62ff",
     "url": "/ReactPortfolio/static/css/main.b417bb56.chunk.css"
   },
   {
-    "revision": "98d318ea3c6c9699c2d4",
-    "url": "/ReactPortfolio/static/js/2.c4f36119.chunk.js"
+    "revision": "e2fb2f3d1b80a89a8580",
+    "url": "/ReactPortfolio/static/js/2.980cdd5d.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/ReactPortfolio/static/js/2.c4f36119.chunk.js.LICENSE.txt"
+    "url": "/ReactPortfolio/static/js/2.980cdd5d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "be652f4ad7e55207b1b7",
-    "url": "/ReactPortfolio/static/js/main.b64761f0.chunk.js"
+    "revision": "566e34d551d8a8cf62ff",
+    "url": "/ReactPortfolio/static/js/main.2b1ab2ce.chunk.js"
   },
   {
     "revision": "0114f5eceb8464d5d956",
